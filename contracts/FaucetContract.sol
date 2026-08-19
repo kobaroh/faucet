fd544cf6f1be479e0ba13140f37e0b2414c55e66
kobaroh <orupetekobaro@gmail.com>
2026-08-19 18:17:16 +0100

    changes

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,6 +2,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    
+
+    uint public numOfFunders;
+    mapping(uint => address) private funders;
+
     receive() external payable {}
+
+    function addFunds() external payable {
+        uint index = numOfFunders++;
+        funders[index] = msg.sender;
+    }
+
+    function getAllFunders() public view returns (address[] memory) {
+        return funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns (address) { 
+    address[] memory _funders = getAllFunders();
+    return funders[index]; 
+    }
+
+    
 }
