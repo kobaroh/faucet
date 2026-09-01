c8f2d5d165c633200cc83dfd5bddd8b195a02701
kobaroh <orupetekobaro@gmail.com>
2026-09-01 11:14:54 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="faucet-wrapper">
+      <div className="faucet"></div>
+      <div className="balance-view is-size-2">
+        Current Balance: <strong>10</strong> ETH
+      </div>
+      <button className="btn mr-4">Donate</button>
+      <button className="btn">Withdraw</button>
     </div>
   );
 }
