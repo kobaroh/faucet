1cb6b30e40183b2ccb2c0dff7d23e23e2641afae
kobaroh <orupetekobaro@gmail.com>
2026-09-02 14:20:24 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import "./App.css";
+import Web3 from "web3";
 
 function App() {
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      let provider = null;
+      
+      if (window.ethereum) {
+        provider = window.ethereum;
+      } 
+      else if (window.web3) {
+        provider = window.web3.currentProvider
+      }
+        else if (!process.env.production) {
+          provider = new Web3.providers.HttpProvider("http://localhost:7545")
+        }
+    }
+
+    loadProvider();
+  }, [])
+
   return (
     <div className="faucet-wrapper">
-      <div className="faucet"></div>
       <div className="balance-view is-size-2">
         Current Balance: <strong>10</strong> ETH
       </div>
-      <button className="btn mr-4">Donate</button>
-      <button className="btn">Withdraw</button>
+      <div className="buttons-row">
+        <button
+          className="button mr-2"
+          onClick={async () => {
+            const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+            console.log(accounts)
+          }}
+        >
+          Enable Ethereum
+        </button>
+        <button className="button is-primary is-small mr-4">Donate</button>
+        <button className="button is-danger is-small">Withdraw</button>
+      </div>
     </div>
   );
 }
