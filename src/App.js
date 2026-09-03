367cb229b3aa80ed5dc53255b659ca117bae449b
kobaroh <orupetekobaro@gmail.com>
2026-09-03 11:30:15 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import "./App.css";
 import Web3 from "web3";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  })
+  const [account, setAccount] = useState(null)
 
   useEffect(() => {
     const loadProvider = async () => {
       let provider = null;
-      
+
       if (window.ethereum) {
         provider = window.ethereum;
+
+        try {
+          await provider.enable(); 
+        } catch {
+          console.error("User denied accounts access!")
+        }
       } 
       else if (window.web3) {
         provider = window.web3.currentProvider
@@ -17,26 +28,37 @@ function App() {
         else if (!process.env.production) {
           provider = new Web3.providers.HttpProvider("http://localhost:7545")
         }
+
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider
+        })
     }
 
     loadProvider();
   }, [])
 
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts()
+      setAccount(accounts[0])
+    }
+
+    web3Api.web3 && getAccount()
+  }, [web3Api.web3])
+
   return (
     <div className="faucet-wrapper">
-      <div className="balance-view is-size-2">
+      <div className="faucet">
+        <span>
+          <strong>Account: </strong>
+        </span>
+        <h1>
+          { account ? account : "not connected" }
+        </h1>
+        <div className="balance-view is-size-2">
         Current Balance: <strong>10</strong> ETH
-      </div>
-      <div className="buttons-row">
-        <button
-          className="button mr-2"
-          onClick={async () => {
-            const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
-            console.log(accounts)
-          }}
-        >
-          Enable Ethereum
-        </button>
+        </div>
         <button className="button is-primary is-small mr-4">Donate</button>
         <button className="button is-danger is-small">Withdraw</button>
       </div>
