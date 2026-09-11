9ff36ff5d964315d6f2a16595df258f155f35ff4
kobaroh <orupetekobaro@gmail.com>
2026-09-11 16:50:08 +0100

    changes

--- src/utils/load-contract.js
+++ src/utils/load-contract.js
@@ -3,4 +3,8 @@
 export const loadContract = async (name) => {
     const res = await fetch(`/contracts/${name}.json`)
     const Artifact = await res.json()
+
+    return {
+        contract: Artifact 
+    }
 }
